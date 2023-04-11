Fixed bug in scattering_angle_PM_contribution_scL_resum where was using the PM order of the contribution to j0 not the overall PM factor of the calculation

weak_field_scattering.py
@@ -15,11 +15,11 @@ def scattering_angle_PM(m1, m2, E, J, PM_order=4):
     if nu_gamma_PM_conditions(nu, gamma, PM_order):
         return 0
     
-    M = m1 + m2
-    nu = m1*m2 / pow(M,2)
+    M = m1 + m2                                         # Sum of masses
+    nu = m1*m2 * pow(M,-2)                               # Symmetric mass ratio
 
-    gamma = 1 + (-1 + pow(E,2)/pow(M,2))/(2.*nu)
-    j = J/(m1*m2)
+    gamma = 1 + (-1 + pow(E,2)/pow(M,2))/(2.*nu)        # Relative Lorentz factor
+    j = J/(m1*m2)                                       # rescaled angular momentum
 
     angle = 0
 
@@ -38,19 +38,21 @@ def scattering_angle_PM_scL_resum(m1, m2, E, J, PM_order=4):
     E = centre of mass total energy
     J = centre of mass angular momentum
     """
-    if nu_gamma_PM_conditions(nu, gamma, PM_order):
-        return 0
     
     M = m1 + m2                                         # Sum of masses
-    nu = m1*m2 / pow(M,2)                               # Symmetric mass ratio
+    nu = m1*m2 * pow(M,-2)                               # Symmetric mass ratio
 
     gamma = 1 + (-1 + pow(E,2)/pow(M,2))/(2.*nu)        # Relative Lorentz factor
     j = J/(m1*m2)                                       # rescaled angular momentum
 
+    if nu_gamma_PM_conditions(nu, gamma, PM_order):
+        return 0
+
     angle = 0
 
     for i in range(1,PM_order+1):
-        angle += scattering_angle_PM_contribution_scL_resum(nu, gamma, i) / pow(j,i)
+        angle += scattering_angle_PM_contribution_scL_resum(nu, gamma, i, PM_order) / pow(j,i)
+        print(angle)
 
     return 2 * scL(critical_rescaled_angular_momentum_PM(nu, gamma, PM_order) / j) * angle
 
@@ -85,7 +87,7 @@ def scattering_angle_PM_contribution(nu, gamma, PM_order):
     
 
 
-def scattering_angle_PM_contribution_scL_resum(nu, gamma, PM_order):
+def scattering_angle_PM_contribution_scL_resum(nu, gamma, PM_contribution_order, PM_order):
     """
     Function to return the PM coefficient of the scattering angle at a given PM order for the resummed in scL
     Expansion defined in arXiv:2211.01399v2 (4.6)
@@ -93,27 +95,32 @@ def scattering_angle_PM_contribution_scL_resum(nu, gamma, PM_order):
 
     nu = m1 m2 / (m1 + m2)^2 = symmetric mass ratio 
     gamma = relative Lorentz factor
+
+    PM_contribution_order is the order of the contribution requested
+    PM_order is the order of the final result (which is needed of j0)
+
     """
     if nu_gamma_PM_conditions(nu, gamma, PM_order):
         return 0
 
-    if PM_order == 1:
+    if PM_contribution_order > PM_order:
+        print('Error: PM_contribution_order is greater than PM_order in scattering_angle_PM_contribution_scL_resum')
+    
+    j0 = critical_rescaled_angular_momentum_PM(nu, gamma, PM_order)
+
+    if PM_contribution_order == 1:
         return scattering_angle_PM_contribution(nu, gamma, 1)
 
-    if PM_order == 2:
-        chi_2 = - 0.5*critical_rescaled_angular_momentum_PM(nu, gamma, 2) * scattering_angle_PM_contribution(nu, gamma, 1)
+    if PM_contribution_order == 2:
+        chi_2 = - 0.5*j0 * scattering_angle_PM_contribution(nu, gamma, 1)
         return chi_2 + scattering_angle_PM_contribution(nu, gamma, 2)
 
-    if PM_order == 3:
-        j0 = critical_rescaled_angular_momentum_PM(nu, gamma, 3)
-
+    if PM_contribution_order == 3:
         chi_3 = - pow(j0,2) / 12. * scattering_angle_PM_contribution(nu, gamma, 1)
         chi_3 += - 0.5 * j0 * scattering_angle_PM_contribution(nu, gamma, 2)
         return chi_3 + scattering_angle_PM_contribution(nu, gamma, 3)
     
-    if PM_order == 4:
-        j0 = critical_rescaled_angular_momentum_PM(nu, gamma, 4)
-
+    if PM_contribution_order == 4:
         chi_4 += - pow(j0,3) / 24. * scattering_angle_PM_contribution(nu, gamma, 1)
         chi_4 += - pow(j0,2) / 12. * scattering_angle_PM_contribution(nu, gamma, 2)
         chi_4 += - 0.5 * j0 * scattering_angle_PM_contribution(nu, gamma, 3)
@@ -186,7 +193,7 @@ def critical_rescaled_angular_momentum_PM(nu, gamma, PM_order):
         return 0
 
     j0 = PM_order * scattering_angle_PM_contribution(nu, gamma, PM_order)/scattering_angle_PM_contribution(nu, gamma, 1)
-    return pow(j0, 1/(PM_order-1))
+    return pow(j0, 1/(PM_order-1.))
 
 
 
@@ -199,7 +206,7 @@ def scL(x):
     return 1/x * np.log(1/(1-x))
 
 
-def nu_gamma_PM_conditions(nu, gamma, PM_order) -> bool:
+def nu_gamma_PM_conditions(nu, gamma, PM_order):
     """
     Function to check values of nu, gamma and PM_order
     Return FALSE if any errors detected